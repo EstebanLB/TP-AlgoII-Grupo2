0d7f8564619a1aef48f97cd4ef94dacee4ac9d8e
EstebanLB <stinkynet@hotmail.com>
2015-10-20 21:43:20 -0300

    fix de tablero

--- src/tp/framework/elementos/Tablero.java
+++ src/tp/framework/elementos/Tablero.java
@@ -1,9 +1,9 @@
 package tp.framework.elementos;
 
-public abstract class Tablero
+public class Tablero
 {
-	//aca no me deja usar abstract para una matriz
-	private Ficha[][]matriz = new Ficha [0][0];
+
+	private  Ficha[][]matriz = new Ficha [0][0];
 
 	public void posicionar(Ficha ficha, int x, int y)
 	{
--- src/tp/framework/juego/config/MotorConfig.java
+++ src/tp/framework/juego/config/MotorConfig.java
@@ -1,6 +1,7 @@
 package tp.framework.juego.config;
 
 import tp.framework.juego.Juego;
+import tp.framework.elementos.Tablero;
 
 public class MotorConfig
 {
